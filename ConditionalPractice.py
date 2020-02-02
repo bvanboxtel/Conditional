0af02bbb18ea8470a9e17278fe6606573b31ce08
bvanboxtel <57278831+bvanboxtel@users.noreply.github.com>
2020-02-02 13:52:52 -0600

    Initial commit

---
+++ ConditionalPractice.py
@@ -0,0 +1,16 @@
+'''
+Created on Feb 1, 2020
+
+@author: ITAUser
+'''
+x = 1
+
+if( x == 1):
+    print("Something")
+    
+if(x == 0):
+    print("Some")
+elif(x == 1):
+    print("thing")
+else:
+    print("N/A")
